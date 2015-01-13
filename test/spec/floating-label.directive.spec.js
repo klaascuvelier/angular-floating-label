cedab667b2603e4c11da5e59246452b25a88c738
Klaas Cuvelier <cuvelierklaas@gmail.com>
2015-01-13 19:16:16 +0100

    Tests - getting started

---
+++ test/spec/floating-label.directive.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('floating-label directive', function () {
+
+    beforeEach(module('components'));
+
+    it('should exist', function () {
+        expect(true).toBeTruthy();
+    });
+
+});
